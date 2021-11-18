6b98bc7c848569873b8b4d4b5b985d005ae3a312
Sandesh <sandeshbansal24@gmail.com>
2021-11-19 00:12:15 +0530

    commit

---
+++ LED.ino
@@ -0,0 +1,46 @@
+
+
+void setup() {
+  // put your setup code here, to run once:
+pinMode(redLED,OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+digitalWrite(redLED,HIGH);
+delay(dit);
+digitalWrite(redLED,LOW);
+delay(dit);
+digitalWrite(redLED,HIGH);
+delay(dit);
+digitalWrite(redLED,LOW);
+delay(dit);
+digitalWrite(redLED,HIGH);
+delay(dit);
+digitalWrite(redLED,LOW);
+delay(dit);
+digitalWrite(redLED,HIGH);
+delay(dah);
+digitalWrite(redLED,LOW);
+delay(dah);
+digitalWrite(redLED,HIGH);
+delay(dah);
+digitalWrite(redLED,LOW);
+delay(dah);
+digitalWrite(redLED,HIGH);
+delay(dah);
+digitalWrite(redLED,LOW);
+delay(dah);
+digitalWrite(redLED,HIGH);
+delay(dit);
+digitalWrite(redLED,LOW);
+delay(dit);
+digitalWrite(redLED,HIGH);
+delay(dit);
+digitalWrite(redLED,LOW);
+delay(dit);
+digitalWrite(redLED,HIGH);
+delay(dit);
+digitalWrite(redLED,LOW);
+delay(dit);
+}
